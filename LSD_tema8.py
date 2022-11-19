f83b5d2d66da12c4baaa98c872b7a403ffb3dc2b
filippaulescu <115627082+filippaulescu@users.noreply.github.com>
2022-11-19 19:38:35 +0200

    Create LSD_tema8.py

---
+++ LSD_tema8.py
@@ -0,0 +1,43 @@
+cheie = "cheie"
+stanga = "stanga"
+dreapta = "dreapta"
+
+nod1 = {cheie: 1, stanga: None, dreapta: None}  # frunza
+nod7 = {cheie: 7, stanga: None, dreapta: None}  # frunza
+nod4 = {cheie: 4, stanga: None, dreapta: None}  # frunza
+nod5 = {cheie: 5, stanga: nod4, dreapta: nod7}
+radacina = {cheie: 2, stanga: nod1, dreapta: nod5}  # = arborele binar
+
+
+
+def afisare_inordine(arbore):
+    if arbore is not None:
+        afisare_inordine(arbore[stanga])
+        print(arbore[cheie], end=" ")
+        afisare_inordine(arbore[dreapta])
+
+
+print("afisare inordine:", end="")
+afisare_inordine(radacina)
+
+'''
+1. Scrieți o funcție care ia un arbore binar și returnează lista nodurilor care au un singur fiu. Ordinea nodurilor
+în listă va fi cea din traversarea în inordine.
+            2
+           /\
+          1  5
+             /\
+            4  7
+'''
+
+print("\n")
+def singur_fiu(arbore):
+    if arbore is not None:
+        singur_fiu(arbore[stanga])
+        if (arbore[stanga] is None) and (arbore[dreapta] is not None) or (arbore[stanga] is not None) and (arbore[dreapta] is  None) :
+            print(arbore[cheie], end=" ")
+        singur_fiu(arbore[dreapta])
+
+
+print("singur fiu:", end="")
+singur_fiu(radacina)
