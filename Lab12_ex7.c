aed59ab8cdaebd2dad2de2c4558abe102c2bfbb8
filippaulescu <115627082+filippaulescu@users.noreply.github.com>
2023-01-23 16:27:58 +0200

    Update Lab12_ex7.c

--- Lab12_ex7.c
+++ Lab12_ex7.c
@@ -3,47 +3,6 @@
 #include <string.h>
 #include <ctype.h>
  
-char *functie( char *string)
-{
-	int len = strlen(string);
-	int c1=0, c2=0;
-	char ch1, ch2;
-	char char_hex[2];
- 
-	char *rezultat=NULL;
-	char *temp=NULL;
- 
-	for( int i=0; i< len; i++ ) 
-	{
-		c1= string[i] / 16;
-		c2= string[i] % 16;
-		ch1= '0' + c1;
-		if ( c2 < 10 ) 
-			ch2 = '0' + c2;
-		else
-			ch2 = 'A' + ( c2 - 10) ;
- 
-		char_hex[0]= ch1;
-		char_hex[1]= ch2;
- 
-		if( (temp=(char *)realloc( rezultat, sizeof(char)*(i+3) )) == NULL)
-		{
-			perror("Eroare alocare memorie");
-			free(rezultat);
-			exit(EXIT_FAILURE);
-		}
- 
-		rezultat=temp;
- 
-		strcat( rezultat, char_hex);
- 
-	}
- 
- 
-	return rezultat;
- 
-}
- 
 char *hex2text(const char *hexstring)
 {
 	int len = strlen(hexstring);
